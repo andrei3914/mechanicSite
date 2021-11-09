a4dc11e7cfcf2a79c889cbd95ac98116b9bb82c3
andrei3914 <popandrei230@yahoo.com>
2021-11-09 15:43:19 +0200

    rezolvat si serviciile

--- src/components/Servicii/Servicii.js
+++ src/components/Servicii/Servicii.js
@@ -4,8 +4,30 @@ import './Servicii.css';
 const Servicii = () => {
 	return (
 		<div className="dimensiuniServicii" id="serviciiFrate">
-			<p> Servicii </p>
-			<p> to be continued </p>
+			<h1 className="f2 pa3"> Servicii </h1>
+			<div className="container">
+				
+				<div class="card center">
+					<div class="card-content">
+						<h2 class="card-title"> Probleme turbine </h2>
+					</div>
+				</div>
+				<div class="card center imagine1">
+					<div class="card-content">
+						<h2 class="card-title"> Probleme injectoare </h2>
+					</div>
+				</div>
+				<div class="card center imagine2">
+					<div class="card-content">
+						<h2 class="card-title"> Pompe de injectie </h2>
+					</div>
+				</div>
+				<div class="card center imagine3">
+					<div class="card-content">
+						<h2 class="card-title"> Schimburi consumabile </h2>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
